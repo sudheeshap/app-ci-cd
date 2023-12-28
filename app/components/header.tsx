35150d704b53f78226aea2aa4ba5897bd455a73f
Sudheesh Parambil <hellosudhee@gmail.com>
2023-12-28 11:31:29 +0400

    feat: header updated

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,8 +2,28 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="bg-slate-400 p-20 w-full text-center" data-testid="header">
-      header here..
+    <div
+      className="bg-slate-400 py-5 w-full text-center m-0"
+      data-testid="header"
+    >
+      <ul className="flex justify-center items-center">
+        <li>
+          <button className="bg-slate-200 rounded-lg p-3 m-2">
+            Restaurants
+          </button>
+        </li>
+        <li>
+          <button className="bg-slate-200 rounded-lg p-3 m-2">Contacts</button>
+        </li>
+        <li>
+          <button className="bg-slate-200 rounded-lg p-3 m-2">Rewards</button>
+        </li>
+        <li>
+          <button className="bg-slate-200 rounded-lg p-3 m-2">
+            Subscription
+          </button>
+        </li>
+      </ul>
     </div>
   );
 };
