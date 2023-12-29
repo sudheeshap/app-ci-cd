c9bb49cfe88a33c94e28f0921f378bbd94ece13e
Sudheesh Parambil <hellosudhee@gmail.com>
2023-12-29 10:46:22 +0400

    chore: deploy to vercel

--- scripts/upload-coverage.sh
+++ scripts/upload-coverage.sh
@@ -5,8 +5,3 @@ npm install
 npm test -- --coverage
 bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r ./coverage/lcov.info
 echo "Uploading coverage Done!"
-# npm run build
-echo "Deploying app to Vercel.."
-npm install -g vercel
-vercel --prod
-echo "Deploying to Vercel Done!"
