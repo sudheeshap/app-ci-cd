340e94880a9275b330636c9b6c43c4aefe3b0779
Sudheesh Parambil <hellosudhee@gmail.com>
2023-12-29 12:12:25 +0400

    chore: deploy to vercel 2

--- scripts/deploy-vercel.sh
+++ scripts/deploy-vercel.sh
@@ -2,9 +2,9 @@
 echo "Install Vercel CLI ${VERCEL_TOKEN}"
 npm install --global vercel@latest
 echo "Pull Vercel Environment Information"
-vercel pull --yes --environment=preview --token=${{ VERCEL_TOKEN }}
+vercel pull --yes --environment=preview --token=${VERCEL_TOKEN}
 echo "Build Project Artifacts"
-vercel build --token=${{ VERCEL_TOKEN }}
+vercel build --token=${VERCEL_TOKEN}
 echo "Deploy Project Artifacts to Vercel"
-vercel deploy --prebuilt --token=${{ VERCEL_TOKEN }}
+vercel deploy --prebuilt --token=${VERCEL_TOKEN}
 echo "Deploying to Vercel Done!"
